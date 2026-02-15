09108ca16dd6789156ff66e9a2251bfad659b340
AlexIn1309 <moran.graciano.cesar@gmail.com>
2026-02-15 04:15:57 -0600

    imprimir variables

---
+++ variables.cs
@@ -0,0 +1,11 @@
+using System;
+
+class HelloWorld
+{
+    static void Main(String[] args)
+    {
+      int edad = 28;
+      string nombre = "Diego";
+        Console.WriteLine("Mi edad es " + edad + " y mi nombre es " + nombre);
+    }
+}
